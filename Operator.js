0c1b29dbdfa8a612b137609b7429fd96ae33b3dc
Makvana Sagar <134779010+MakvanaSagar@users.noreply.github.com>
2025-08-02 21:55:33 +0530

    Add files via upload

---
+++ Operator.js
@@ -0,0 +1,51 @@
+/*Arithmetic Operators Example
+
+let s = 19;
+let d = (100+100) * s;
+console.log(d);
+
+*/
+
+
+
+
+/*Assignment Operators Example
+
+let i = 99;
+i += 1;
+console.log(i)
+
+*/
+
+
+/*Comparison Operators Example
+
+let u = 9;
+if(u>=8.99){
+	console.log("Yes")
+}
+else{
+	console.log("No")
+}
+
+*/
+
+
+/*Logical Operators Example
+
+let age = 25;
+if (age > 18 && age < 30) {
+  console.log("You are a young adult.");
+}
+
+*/
+
+
+
+/*Bitwise Operators Example
+
+console.log(5 | 3);
+console.log(5 & 3);
+console.log(~3);
+
+*/
